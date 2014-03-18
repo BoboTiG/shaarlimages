cb4df52ab12a2c15cf464a06ddf89218af876238
BoboTiG <bobotig@gmail.com>
2014-03-18 16:59:23 +0100

    Fix PHP Notice:  getimagesize(): Read error! in inc/Functions.php on line 188

--- inc/Functions.php
+++ inc/Functions.php
@@ -181,23 +181,25 @@ class Fct
             foreach ( glob(Config::$cache_dir.'*.php') as $db )
             {
                 $tmp = self::unserialise($db);
-                unset($tmp['date']);
-                foreach ( array_keys($tmp) as $key )
-                {
-                    if ( is_file(Config::$img_dir.$tmp[$key]['link']) ) {
-                        /* RATIO limit */list($width, $height) = getimagesize(Config::$img_dir.$tmp[$key]['link']);
-                        /* RATIO limit */if ( $height == 0 ) $height = 1;
-                        /* RATIO limit */$ratio = $width / $height;
-                        /* RATIO limit */if ( $ratio >= 0.3 && $ratio <= 3.0 )
-                        /* RATIO limit */{
-                            if ( empty($images[$key]) ) {
-                                $images[$key] = $tmp[$key];
-                            }
-                            elseif ( $tmp[$key]['date'] < $images[$key]['date'] ) {
-                                // Older is better (could be the first to share)
-                                $images[$key] = $tmp[$key];
-                            }
-                        /* RATIO limit */}
+                if ( $tmp !== false ) {
+                    unset($tmp['date']);
+                    foreach ( array_keys($tmp) as $key )
+                    {
+                        if ( is_file(Config::$img_dir.$tmp[$key]['link']) ) {
+                            /* RATIO limit */list($width, $height) = getimagesize(Config::$img_dir.$tmp[$key]['link']);
+                            /* RATIO limit */if ( $height == 0 ) $height = 1;
+                            /* RATIO limit */$ratio = $width / $height;
+                            /* RATIO limit */if ( $ratio >= 0.3 && $ratio <= 3.0 )
+                            /* RATIO limit */{
+                                if ( empty($images[$key]) ) {
+                                    $images[$key] = $tmp[$key];
+                                }
+                                elseif ( $tmp[$key]['date'] < $images[$key]['date'] ) {
+                                    // Older is better (could be the first to share)
+                                    $images[$key] = $tmp[$key];
+                                }
+                            /* RATIO limit */}
+                        }
                     }
                 }
             }
