e85257f31848f1178be94f6b219cfd4b4ea1f8f0
Mickaël Schoentgen <contact@tiger-222.fr>
2023-10-06 16:05:05 +0200

    fix: Adjust the `$PATH` to ease my life

--- host/__main__.py
+++ host/__main__.py
@@ -12,6 +12,10 @@ Options:
     --index=<idx> Feed index to sync.
     --force       Force resync.
 """
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent))
 
 if __name__ == "__main__":
     import functions
