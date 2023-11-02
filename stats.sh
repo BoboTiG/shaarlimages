fac850460ce2b8e0b8ec8964ab8a4b9f6f1d0fb0
Mickaël Schoentgen <contact@tiger-222.fr>
2023-11-03 00:50:08 +0100

    fix: Use real prices in stats

--- stats.sh
+++ stats.sh
@@ -2,4 +2,9 @@
 echo "- images  : $(/bin/find data/images -type f | /bin/wc -l)"
 echo "- shaarlis: $(/bin/find data/feeds -type f | /bin/wc -l) / $(/bin/grep -c http data/shaarlis.json)"
 echo "- storage : $(/bin/du -sh data | /bin/sed 's/data//')"
-echo "- price   : $((10*12)) €/year (or $((10*12/12)) €/month)"
+
+# Update on 2023-11-03
+pythonanywhere=8.48
+infomaniak=$(/bin/echo '15.84/12' | /bin/bc)
+monthly=$(/bin/echo "${pythonanywhere}+${infomaniak}" | /bin/bc)
+echo "- price   : $(/bin/echo "${monthly}*12" | /bin/bc) €/year (or ${monthly} €/month)"
