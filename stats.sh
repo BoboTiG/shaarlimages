d31dca248942a8b7303e0640e3906343e8f791ac
Mickaël Schoentgen <contact@tiger-222.fr>
2023-11-03 20:39:42 +0100

    fix: stats + update

--- stats.sh
+++ stats.sh
@@ -3,8 +3,8 @@ echo "- images  : $(/bin/find data/images -type f | /bin/wc -l)"
 echo "- shaarlis: $(/bin/find data/feeds -type f | /bin/wc -l) / $(/bin/grep -c http data/shaarlis.json)"
 echo "- storage : $(/bin/du -sh data | /bin/sed 's/data//')"
 
-# Update on 2023-11-03
-pythonanywhere=8.48
-infomaniak=$(/bin/echo '15.84/12' | /bin/bc)
-monthly=$(/bin/echo "${pythonanywhere}+${infomaniak}" | /bin/bc)
-echo "- price   : $(/bin/echo "${monthly}*12" | /bin/bc) €/year (or ${monthly} €/month)"
+# Updated on 2023-11-03
+pythonanywhere=8.95
+infomaniak=$(python3 -c 'print(15.84 / 12)')
+monthly=$(python3 -c "print(${pythonanywhere} + ${infomaniak})")
+echo "- price   : $(python3 -c "print(${monthly} * 12)") €/year (or ${monthly} €/month)"
